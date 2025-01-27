dd5397e16191921baabac92853c35c5691f930dc
RobertSandiford <mail@robertsandiford.co.uk>
2025-01-27 06:58:40 +0000

    add more exapmles, run node even if tsc errors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 
-import { loadStripe } from '@stripe/stripe-js/pure'
+// TS says it is OK, but actually fails a runtime
+import { loadStripe as loadStripe1 } from '@stripe/stripe-js/pure'
 
-console.log(loadStripe) // prevent unused import from being stripped
+// imports a CJS file, works
+import { loadStripe as loadStripe2 } from '@stripe/stripe-js/pure/index.js'
+
+// the pure/index.mjs is invalid because it tries to import a file without specifying the extension
+// tsc detects this and reports an error
+import { loadStripe as loadStripe3 } from '@stripe/stripe-js/pure/index.mjs'
+
+console.log(loadStripe1) // prevent unused import from being stripped
+console.log(loadStripe2) // prevent unused import from being stripped
+console.log(loadStripe3) // prevent unused import from being stripped
